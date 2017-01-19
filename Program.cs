d8cdb6cd00f7aa5a78165261cd380bee4bfbbc1d
Anton Tomov <AntonioTomo01@gmail.com>
2017-01-19 09:09:34 -0800

    Add files via upload

---
+++ Program.cs
@@ -0,0 +1,45 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace Hospital
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            int numDays = int.Parse(Console.ReadLine());
+            int doctors = 7;
+            int treatedPatients = 0;
+            int untreatedPatients = 0;
+
+            for (int i = 1; i <= numDays; i++)
+            {
+                if (i % 3 == 0)
+                {
+                    if (untreatedPatients > treatedPatients)
+                    {
+                        doctors++;
+                    }
+                }
+
+                int patients = int.Parse(Console.ReadLine());
+
+                if (patients > doctors)
+                {
+                    untreatedPatients += (patients - doctors);
+                    treatedPatients += doctors;
+                }
+                else if (patients <= doctors)
+                {
+                    treatedPatients += patients;
+                }
+            }
+
+            Console.WriteLine("Treated patients: {0}.", treatedPatients);
+            Console.WriteLine("Untreated patients: {0}.", untreatedPatients);
+        }
+    }
+}
